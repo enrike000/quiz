d2a3a47427c006106fdece8e9940b5209393d6e8
enrike000 <blooder412@gmail.com>
2022-02-03 14:26:05 +0400

    Update quizapp.jsx

--- src/quizapp.jsx
+++ src/quizapp.jsx
@@ -3,39 +3,39 @@ import { useState } from "react";
 const Quiz = () => {
   const questions = [
     {
-      shekitxva: "ramdeni wlisaa dzroxa",
-      kitxvisoption: [
-        { pasuxi: "7", iscorect: true },
-        { pasuxi: "71", iscorect: false },
-        { pasuxi: "31", iscorect: false },
-        { pasuxi: "31", iscorect: false },
+      qtext: "Where is Georgia located?",
+      answers: [
+        { atext: "asia", correct: false },
+        { atext: "europe", correct: true },
+        { atext: "africa", correct: false },
+        { atext: "south america", correct: false },
       ],
     },
     {
-      shekitxva: "ramdeni wlisaa cxeni",
-      kitxvisoption: [
-        { pasuxi: "74", iscorect: true },
-        { pasuxi: "71", iscorect: false },
-        { pasuxi: "31", iscorect: false },
-        { pasuxi: "31", iscorect: false },
+      qtext: "Where does the zebra live?",
+      answers: [
+        { atext: "in the moon", correct: false },
+        { atext: "in the sun", correct: false },
+        { atext: "in the star", correct: false },
+        { atext: "in the bag", correct: true },
       ],
     },
     {
-      shekitxva: "ramdeni wlisaa iviri",
-      kitxvisoption: [
-        { pasuxi: "73", iscorect: true },
-        { pasuxi: "71", iscorect: false },
-        { pasuxi: "31", iscorect: false },
-        { pasuxi: "31", iscorect: false },
+      qtext: "Which is the fastest ",
+      answers: [
+        { atext: "BMW", correct: false },
+        { atext: "MERCEDES", correct: false },
+        { atext: "HYUNDAI GENESIS COUPE", correct: true },
+        { atext: "PEUGEOT", correct: false },
       ],
     },
     {
-      shekitxva: "ramdeni wlisaa fefefe",
-      kitxvisoption: [
-        { pasuxi: "17", iscorect: true },
-        { pasuxi: "71", iscorect: false },
-        { pasuxi: "31", iscorect: false },
-        { pasuxi: "31", iscorect: false },
+      qtext: "Which is the best game",
+      answers: [
+        { atext: "LOL", correct: false },
+        { atext: "GTA", correct: false },
+        { atext: "D O T A", correct: true },
+        { atext: "fifa", correct: false },
       ],
     },
   ];
